63ba92f11833a94b49fcf9748a1d1b29bd68b1fe
divs bhalala <bhalaladivs@gmail.com>
2017-06-27 11:35:20 +0530

    refactoring Deiplay page

--- tools/postcss.config.js
+++ tools/postcss.config.js
@@ -1,11 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
 
 module.exports = () => ({
   // The list of plugins for PostCSS
