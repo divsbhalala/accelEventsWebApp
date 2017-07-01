5b7fd26c184182f7e67424a4fb79dc51b43de0ec
divsbhalala <bhalaladivs@gmail.com>
2017-07-01 19:04:04 +0530

    added timer countdown

--- src/components/EventAside/EventAside.js
+++ src/components/EventAside/EventAside.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import PropTypes   from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
@@ -33,11 +32,17 @@ class EventAside extends React.Component {
 			showBuyRaffelTicketPopup: false,
 			filterCategory: '',
 			serachSring: '',
-      isshowBuyRaffleTicketsModal:false,
+			isshowBuyRaffleTicketsModal: false,
+			days: 0,
+			hours: 0,
+			minute: 0,
+			seconds: 0,
 
 		};
 		this.showBuyRaffelTicketPopup = this.showBuyRaffelTicketPopup.bind(this);
 		this.hideBuyRaffelTicketPopup = this.hideBuyRaffelTicketPopup.bind(this);
+		this.setCountDown = this.setCountDown.bind(this);
+
 	}
 
 	showBuyRaffelTicketPopup = () => {
@@ -52,28 +57,56 @@ class EventAside extends React.Component {
 	};
 	serachString = (e) => {
 		this.props.setSearchString(this.serachKey.value);
-	}
-  showBuyRaffleTicketsModal = () => {
-    this.setState({
-      isshowBuyRaffleTicketsModal: true
-    })
-  };
-  hideBuyRaffleTicketsModal = () => {
-    this.setState({
-      isshowBuyRaffleTicketsModal: false
-    })
-  };
+	};
+	showBuyRaffleTicketsModal = () => {
+		this.setState({
+			isshowBuyRaffleTicketsModal: true
+		})
+	};
+	hideBuyRaffleTicketsModal = () => {
+		this.setState({
+			isshowBuyRaffleTicketsModal: false
+		})
+	};
+	setCountDown=()=>{
+		let interval = 1000;
+		let self=this;
+		setInterval(function(){
+			let eventTime=moment();
+			if(self.props.settings && self.props.settings.endDate){
+				eventTime = moment(self.props.settings.endDate);
+			}
+			let days =  moment(eventTime).diff(moment(), 'days');
+			let hours = moment(eventTime).add(-days, 'days').diff(moment(), 'hours');
+			let minute = moment(eventTime).add(-days, 'days').add(-hours, 'hours').diff(moment(), 'minutes');
+			let seconds = moment(eventTime).add(-days, 'days').add(-hours, 'hours').add(-minute, 'minutes').diff(moment(), 'seconds');
 
+			// let duration = moment.duration(duration - interval, 'milliseconds');
+			self.setState({
+				days: days <= 0 ? "00": days,
+				hours: hours <= 0 ? "00": hours,
+				minute: minute <= 0 ? "00": minute,
+				seconds: seconds <= 0 ? "00": seconds,
+			});
+
+			if( !days && !hours && !minute && !seconds){
+				//self.props.onEnd();
+			}
+		}, interval);
+	};
+	componentDidMount(){
+
+	}
 	render() {
 		return (
 			<div>
 				<BuyRaffleTicketsModal
-          showModal={this.state.isshowBuyRaffleTicketsModal}
-          headerText=""
-          onCloseFunc={this.hideBuyRaffleTicketsModal}
-          params={this.props.params}  />
+					showModal={this.state.isshowBuyRaffleTicketsModal}
+					headerText=""
+					onCloseFunc={this.hideBuyRaffleTicketsModal}
+					params={this.props.params}/>
 				<script type="text/javascript"
-				        src="//maps.google.com/maps/api/js?sensor=false&amp;libraries=places&amp;key=AIzaSyCTdjRtF5L54QIJdEQ8DyXlf2umq6MpvEw"></script>
+								src="//maps.google.com/maps/api/js?sensor=false&amp;libraries=places&amp;key=AIzaSyCTdjRtF5L54QIJdEQ8DyXlf2umq6MpvEw"></script>
 				<div className={cx("main-box", "clearfix")}>
 					<header className={cx("main-box-header", "clearfix")}>
 						<h2>jkazarian8</h2>
@@ -88,16 +121,16 @@ class EventAside extends React.Component {
 						{ this.props.activeTab && (this.props.activeTab == 'The Event' ) &&
 						<div className={cx("the-event", "mrg-t-lg")}>
 							{ this.props.showBookingPopup && <a onClick={this.props.showBookingPopup}
-							                                    className={cx("btn", "btn-block", "btn-lg", "btn-orange", this.props.settings ? "" : "disabled")}>Buy
+																									className={cx("btn", "btn-block", "btn-lg", "btn-orange", this.props.settings ? "" : "disabled")}>Buy
 								Tickets</a>}
 							<div className={cx("box")}>
 								<div className={cx("box-title", "text-uppercase")}>date and time</div>
 								<div className={cx("box-content")}>
 									{this.props.settings && this.props.settings.startDate && <Moment format="ddd MMMM D YYYY, h:mm A"
-									                                                                 tz={this.props.eventData && this.props.eventData.timezoneId}>{this.props.settings.startDate}</Moment>}
+																																									 tz={this.props.eventData && this.props.eventData.timezoneId}>{this.props.settings.startDate}</Moment>}
 									-
 									{this.props.settings && this.props.settings.endDate && <Moment format="ddd MMMM D YYYY, h:mm A"
-									                                                               tz={this.props.eventData && this.props.eventData.timezoneId}>{this.props.settings.endDate}</Moment>}
+																																								 tz={this.props.eventData && this.props.eventData.timezoneId}>{this.props.settings.endDate}</Moment>}
 									<br />
 									<time>
 										<span className="hide"> (America/New_York)</span>
@@ -136,30 +169,19 @@ class EventAside extends React.Component {
 									</div>
 								</div>
 								{ this.props.settings && this.props.settings.endDate && <div className={cx("project-box-content")}>
-									<div className={cx("ticker")}>
+									<div className={cx("ticker")}>{this.setCountDown()}
 										<div className={cx("row timer")}>
-											<div className={cx("col-xs-4")}><span className={cx("days")}>{
-												moment(this.props.settings.endDate).diff(moment(), 'days') > 0
-												&& ( moment(this.props.settings.endDate).diff(moment(), 'days')) || '00'
-											}</span></div>
-											<div className={cx("col-xs-4")}><span className={cx("hours")}>{
-												moment(this.props.settings.endDate).add(-moment(this.props.settings.endDate).diff(moment(), 'days'), 'days').diff(moment(), 'hours') > 0
-												&& moment(this.props.settings.endDate).add(-moment(this.props.settings.endDate).diff(moment(), 'days'), 'days').diff(moment(), 'hours') > 0
-												&& ('0' + moment(this.props.settings.endDate).add(-moment(this.props.settings.endDate).diff(moment(), 'days'), 'days').diff(moment(), 'hours')).slice(-2) || '00'
-											}</span></div>
-											<div className={cx("col-xs-4")}><span className={cx("minutes")}>{
-												moment(this.props.settings.endDate).add(-moment(this.props.settings.endDate).diff(moment(), 'days'), 'days').add(-moment(this.props.settings.endDate).add(-moment(this.props.settings.endDate).diff(moment(), 'days'), 'days').diff(moment(), 'hours'), 'hours').diff(moment(), 'minutes') > 0
-												&& ( "0" + moment(this.props.settings.endDate).add(-moment(this.props.settings.endDate).diff(moment(), 'days'), 'days').add(-moment(this.props.settings.endDate).add(-moment(this.props.settings.endDate).diff(moment(), 'days'), 'days').diff(moment(), 'hours'), 'hours').diff(moment(), 'minutes')).slice(-2)
-												|| '00'}</span></div>
-											<div className={cx("col-xs-4")} style={{display: "none"}}><span
-												className={cx("seconds")}>00</span></div>
+											{ this.state.days > 0 && <div className={cx("col-xs-4")}><span className={cx("days")}>{this.state.days}</span></div>}
+											{ <div className={cx("col-xs-4")}><span className={cx("days")}>{this.state.hours}</span></div>}
+											{ <div className={cx("col-xs-4")}><span className={cx("days")}>{this.state.minute}</span></div>}
+											{ this.state.days <= 0 && <div className={cx("col-xs-4")}><span className={cx("days")}>{this.state.seconds}</span></div>}
+
 										</div>
 										<div className={cx("row tiny text-center")}>
-											<div className={cx("col-xs-4")}><span className={cx("days")}>DAYS</span></div>
+											{ this.state.days > 0 && <div className={cx("col-xs-4")}><span className={cx("days")}>DAYS</span></div> }
 											<div className={cx("col-xs-4")}><span className={cx("hours")}>HOURS</span></div>
 											<div className={cx("col-xs-4")}><span className={cx("minutes")}>MINUTES</span></div>
-											<div className={cx("col-xs-4")} style={{display: "none"}}><span
-												className={cx("seconds")}>SECONDS</span></div>
+											{ this.state.days <= 0 && <div className={cx("col-xs-4")} ><span className={cx("seconds")}>SECONDS</span></div>}
 										</div>
 									</div>
 								</div>}
@@ -182,29 +204,31 @@ class EventAside extends React.Component {
 						</div> }
 						{ this.props.activeTab && ( this.props.activeTab == 'Raffle' ) && this.props.settings && this.props.settings.endDate &&
 						<a role="button"
-						   className={cx("btn btn-primary btn-block buy-raffle-tickets", ((moment(this.props.settings.endDate).diff(moment()) <= 0 && !this.props.eventData.raffleEnabled) || this.props.settings.moduleEnded)&& 'disabled')}
-						   disabled={(moment(this.props.settings.endDate).diff(moment()) <= 0 && !this.props.eventData.raffleEnabled) || this.props.settings.moduleEnded}
-						   onClick={this.showBuyRaffleTicketsModal}>{ (moment(this.props.settings.endDate).diff(moment()) <= 0 && !this.props.eventData.raffleEnabled) || this.props.settings.moduleEnded ? 'Raffle Closed' : 'Buy Raffle Tickets'}</a> }
+							 className={cx("btn btn-primary btn-block buy-raffle-tickets", ((moment(this.props.settings.endDate).diff(moment()) <= 0 && !this.props.eventData.raffleEnabled) || this.props.settings.moduleEnded) && 'disabled')}
+							 disabled={(moment(this.props.settings.endDate).diff(moment()) <= 0 && !this.props.eventData.raffleEnabled) || this.props.settings.moduleEnded}
+							 onClick={this.showBuyRaffleTicketsModal}>{ (moment(this.props.settings.endDate).diff(moment()) <= 0 && !this.props.eventData.raffleEnabled) || this.props.settings.moduleEnded ? 'Raffle Closed' : 'Buy Raffle Tickets'}</a> }
 						{ this.props.activeTab && !(this.props.activeTab == 'The Event' || this.props.activeTab == 'Donation' ) && this.props.activeCategory &&
 						<div className={cx("search-bar card")}>
 							<input type="text" className={cx("form-control")} placeholder="Search Items..."
-							       onChange={this.serachString} ref={ref => {
-                this.serachKey = ref;
-              }}/>
+										 onChange={this.serachString} ref={ref => {
+								this.serachKey = ref;
+							}}/>
 						</div> }
 						<div className={cx("text-center powered-by-sidebar")}><span>Powered by </span>
 							<Link to="https://www.accelevents.com" target="_blank">
-							<img
-								src="http://v2-dev-images-public.s3-website-us-east-1.amazonaws.com/1-300x50/937320cf-a809-49c5-916d-e7436a1cfcaeaccelevents-logo-black.png"
-								className={cx("img-responsive")}/>
-						</Link>
+								<img
+									src="http://v2-dev-images-public.s3-website-us-east-1.amazonaws.com/1-300x50/937320cf-a809-49c5-916d-e7436a1cfcaeaccelevents-logo-black.png"
+									className={cx("img-responsive")}/>
+							</Link>
 						</div>
 						{ this.props.settings && this.props.settings.categoriesEnabled && this.props.settings.categories && this.props.activeCategory &&
 						<div id="divItemCategories" className={cx("item-categories hidden-xs")}>
 							<h4 className={cx("")}>Categories</h4>
 							<ul className={cx("nav nav-pills nav-stacked category-list ")}>
-								<li className={cx("all-items",this.props.selectedCategoty == '' && "active")}>
-									<a href="#" className={cx("category-switcher all-items")}onClick={()=>{this.props.setFilterCategory("")}}>
+								<li className={cx("all-items", this.props.selectedCategoty == '' && "active")}>
+									<a href="#" className={cx("category-switcher all-items")} onClick={() => {
+										this.props.setFilterCategory("")
+									}}>
 										<i className={cx("fa fa-ticket")}></i>
 										<span className={cx("cat-name")}>All Items</span>
 										<span className={cx("badge badge-primary pull-right cat-count")}></span>
@@ -213,7 +237,9 @@ class EventAside extends React.Component {
 								{
 									this.props.settings && this.props.settings.categories && this.props.settings.categories.map(item =>
 										<li className={cx(this.props.selectedCategoty == item.name && "active")}
-										    key={item.name + Math.random()} onClick={()=>{this.props.setFilterCategory(item.name)}}>
+												key={item.name + Math.random()} onClick={() => {
+											this.props.setFilterCategory(item.name)
+										}}>
 											<a className={cx("category-switcher pointer")}>
 												<i className={cx("fa fa-ticket")}></i>
 												<span className={cx("cat-name")}>{item.name}</span>
@@ -242,7 +268,7 @@ class EventAside extends React.Component {
 					<div className="main-box-body clearfix">
 						<div className="payment-area collapse in">
 							<form className="ajax-form validated fv-form fv-form-bootstrap"
-							      method="post" action="/AccelEventsWebApp/events/12/R/purchaseticket" noValidate="novalidate">
+										method="post" action="/AccelEventsWebApp/events/12/R/purchaseticket" noValidate="novalidate">
 								<button type="submit" className="fv-hidden-submit" style={{display: 'none', width: 0, height: 0}}/>
 								<div className="ajax-msg-box text-center mrg-b-lg" style={{display: 'none'}}><span
 									className="fa fa-spinner fa-pulse fa-fw"/> <span className="resp-message"/></div>
@@ -251,11 +277,11 @@ class EventAside extends React.Component {
 									<div className="input-group">
 										<div className="input-group-addon"><i className="fa fa-ticket" aria-hidden="true"/></div>
 										<select className="form-control" name="pkg" id="ticketpkgs" defaultValue={0}>
-											<option value={0}  disabled>Select Tickets</option>
+											<option value={0} disabled>Select Tickets</option>
 											{this.props.settings && this.props.settings.ticketPackages ?
-												this.props.settings.ticketPackages.map(item=>
+												this.props.settings.ticketPackages.map(item =>
 													<option value={item.id} key={Math.random()} data-ticket={item.numOfTicket}
-													        data-price={item.price}>
+																	data-price={item.price}>
 														{item.numOfTicket} Ticket For ${item.price}
 													</option>
 												) : ''
@@ -269,10 +295,10 @@ class EventAside extends React.Component {
 								<div className="stripe-form">
 								</div>
 								<button type="submit" className="btn btn-green"
-								        data-loading-text="<i class='fa fa-spinner fa-spin'></i> Processing Payment">Pay Now
+												data-loading-text="<i class='fa fa-spinner fa-spin'></i> Processing Payment">Pay Now
 								</button>
 								<button type="button" className="btn btn-default btn-close" data-dismiss="modal" aria-label="Close"
-								        style={{display: 'none'}}>Close
+												style={{display: 'none'}}>Close
 								</button>
 							</form>
 						</div>
