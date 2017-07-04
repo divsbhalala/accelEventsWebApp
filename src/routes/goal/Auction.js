5bb4967520deaa6a68b8054284e1284be80870dc
divs bhalala <bhalaladivs@gmail.com>
2017-07-04 11:42:16 +0530

    resolve scrolling, Goal page issue

--- src/routes/goal/Auction.js
+++ src/routes/goal/Auction.js
@@ -46,7 +46,7 @@ class Auction extends React.Component {
 
 	render() {
 		return (
-			<div className="container goal-page" style={{"margin-bottom":"120px"}}>
+			<div className="container goal-page" style={{"marginBottom":"120px"}}>
 				<div className="row">
 					<div className="col-lg-12">
 						<div>
