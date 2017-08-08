bd7427837b78721821dab585219979ae66b2785c
Colin Shah <Colin.shah1989@gmail.com>
2017-08-08 12:43:59 +0530

    Fund a need >  Log in(link) or Sign up below > 'Close' button is missing from 'Login' popup, which appears when user clicks on 'Log in' link

--- src/routes/event/fund/Fund.js
+++ src/routes/event/fund/Fund.js
@@ -1022,7 +1022,13 @@ class Fund extends React.Component {
             </div>
           </div>
         </PopupModel>
-        <LoginModal showModal={this.state.isShowLoginModal}  	onCloseFunc={this.hideLoginModal}   params={this.props.params && this.props.params.params}/>
+
+        <LoginModal
+             showModal={this.state.isShowLoginModal}
+             onCloseFunc={this.hideLoginModal}
+              params={this.props.params }
+             modelFooter={<button type="button" className="btn btn-info center-block" data-dismiss="modal" onClick={()=>{this.hideLoginModal()}}> Close </button>}
+          />
       </div>
     );
   }
