f54c1ae6d7653a33d78a426fd175141bc0e4e00e
Rohit Rehan <rohitrehan008@gmail.com>
2017-08-08 18:36:20 +0530

    empty line fix

--- src/routes/myProfile/MyProfile.js
+++ src/routes/myProfile/MyProfile.js
@@ -66,40 +66,53 @@ class MyProfile extends React.Component {
 
   render() {
     return (
-      <div className="row myProfile">
-        <div className="col-lg-12">
-          {this.state.user ?
-          <div id="content-wrapper">
-            <div className="row">
-              <div className="col-lg-3 col-md-4 col-sm-4">
-                <ProfileAside setActiveTabState={this.setActiveTabState} user={this.state.user && this.state.user} />
-              </div>
-              <div className="col-lg-9 col-md-8 col-sm-8 ">
-                { this.state.isLoaded && <div className="main-box">
-                  <Tabs
-                    onSelect={(index, label) => {
-                      this.setActiveTabState(label);
-                    }} selected={this.state.activeTab || 'Events'} className="tabs-wrapper"
-                  >
+      <div className="my-profile-wrap">
+        {this.state.user ?
+        <div id="content-wrapper">
+          <div className="row">
+            <div className="col-lg-3 col-md-4 col-sm-4">
+              <ProfileAside setActiveTabState={this.setActiveTabState} user={this.state.user && this.state.user} />
+            </div>
+            <div className="col-lg-9 col-md-8 col-sm-8 ">
+              { this.state.isLoaded && <div className="main-box">
+                <Tabs
+                  onSelect={(index, label) => {
+                    this.setActiveTabState(label);
+                  }} selected={this.state.activeTab || 'Events'} className="tabs-wrapper"
+                >
 
-                    <Tab label="Events">
-                      <div className="row">
-                        <div className="tab-content">
-                          <table className="table table-striped table-hover table-bordered" cellSpacing={0} id="auctionevents">
-                            <thead>
-                              <tr>
-                                <th>Event</th>
-                                <th>END Date</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              {
-                            this.state.user && this.state.user.userEventInfo && this.state.user.userEventInfo.map((item, index) =>
-                              <EventList key={index} item={item} />,
-                            )
-                          }
-                            </tbody>
-                          </table>
+                  <Tab label="Events">
+                    <div className="row">
+                      <div className="tab-content">
+                        <table className="table table-striped table-hover table-bordered" cellSpacing={0} id="auctionevents">
+                          <thead>
+                            <tr>
+                              <th>Event</th>
+                              <th>END Date</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {
+                              this.state.user && this.state.user.userEventInfo && this.state.user.userEventInfo.map((item, index) =>
+                                <EventList key={index} item={item} />,
+                              )
+                            }
+                          </tbody>
+                        </table>
+                        <div>
+                          <form id="newEventForm" method="POST" action="/AccelEventsWebApp/u/create/newevent">
+                            <button type="submit" className="btn btn-default btn-block">
+                              Create New Event
+                            </button>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </Tab>
+                  <Tab label="Profile">
+                    <div className="row">
+                      <div className="tab-content">
+                        <div className="tab-pane fade" id="tab-events">
                           <div>
                             <form id="newEventForm" method="POST" action="/AccelEventsWebApp/u/create/newevent">
                               <button type="submit" className="btn btn-default btn-block">
@@ -108,49 +121,27 @@ class MyProfile extends React.Component {
                             </form>
                           </div>
                         </div>
-                      </div>
-                    </Tab>
-                    <Tab label="Profile">
-                      <div className="row">
-                        <div className="tab-content">
-                          <div className="tab-pane fade" id="tab-events">
-                            <div>
-                              <form id="newEventForm" method="POST" action="/AccelEventsWebApp/u/create/newevent">
-                                <button type="submit" className="btn btn-default btn-block">
-                                  Create New Event
-                                </button>
-                              </form>
-                            </div>
-                          </div>
-                          <div className="tab-pane fade active in" id="tab-profile">
-
-                            <ProfileField title="Firstname" fieldName="firstName" fieldValue={this.state.user && this.state.user.firstName ? this.state.user.firstName : 'Empty' } updatePProfile={this.updateAllProfile}/>
-                            <ProfileField title="LastName" fieldName="lastName" fieldValue={this.state.user && this.state.user.lastName ? this.state.user.lastName : 'Empty' } updatePProfile={this.updateAllProfile}/>
-                            <ProfileField title="Email" fieldName="email" fieldValue={this.state.user && this.state.user.email ? this.state.user.email : 'Empty' } updatePProfile={this.updateAllProfile}/>
-                            <ProfileField title="Phone" fieldName="phoneNumber" fieldValue={this.state.user && this.state.user.phoneNumber ? this.state.user.phoneNumber : 'Empty' } updatePProfile={this.updateAllProfile}/>
-                            <ProfileField title="Address Line 1" fieldName="address1" fieldValue={this.state.user && this.state.user.address1 ? this.state.user.address1 : 'Empty' } updatePProfile={this.updateAllProfile}/>
-                            <ProfileField title="Address Line 2" fieldName="address2" fieldValue={this.state.user && this.state.user.address2 ? this.state.user.address2 : 'Empty' } updatePProfile={this.updateAllProfile}/>
-                            <ProfileField title="State" fieldName="state" fieldValue={this.state.user && this.state.user.state ? this.state.user.state : 'Empty' } updatePProfile={this.updateAllProfile}/>
-                            <ProfileField title="City" fieldName="cityOrProvidence" fieldValue={this.state.user && this.state.user.cityOrProvidence ? this.state.user.cityOrProvidence : 'Empty' } updatePProfile={this.updateAllProfile}/>
-                            <ProfileField title="Country" fieldName="country" fieldValue={this.state.user && this.state.user.country ? this.state.user.country : 'Empty' } updatePProfile={this.updateAllProfile}/>
-                            <ProfileField title="Zip" fieldName="zipcode" fieldValue={this.state.user && this.state.user.zipcode ? this.state.user.zipcode : 'Empty' } updatePProfile={this.updateAllProfile}/>
-
-                          </div>
+                        <div className="tab-pane fade active in" id="tab-profile">
+                          <ProfileField title="Firstname" fieldName="firstName" fieldValue={this.state.user && this.state.user.firstName ? this.state.user.firstName : 'Empty' } updatePProfile={this.updateAllProfile}/>
+                          <ProfileField title="LastName" fieldName="lastName" fieldValue={this.state.user && this.state.user.lastName ? this.state.user.lastName : 'Empty' } updatePProfile={this.updateAllProfile}/>
+                          <ProfileField title="Email" fieldName="email" fieldValue={this.state.user && this.state.user.email ? this.state.user.email : 'Empty' } updatePProfile={this.updateAllProfile}/>
+                          <ProfileField title="Phone" fieldName="phoneNumber" fieldValue={this.state.user && this.state.user.phoneNumber ? this.state.user.phoneNumber : 'Empty' } updatePProfile={this.updateAllProfile}/>
+                          <ProfileField title="Address Line 1" fieldName="address1" fieldValue={this.state.user && this.state.user.address1 ? this.state.user.address1 : 'Empty' } updatePProfile={this.updateAllProfile}/>
+                          <ProfileField title="Address Line 2" fieldName="address2" fieldValue={this.state.user && this.state.user.address2 ? this.state.user.address2 : 'Empty' } updatePProfile={this.updateAllProfile}/>
+                          <ProfileField title="State" fieldName="state" fieldValue={this.state.user && this.state.user.state ? this.state.user.state : 'Empty' } updatePProfile={this.updateAllProfile}/>
+                          <ProfileField title="City" fieldName="cityOrProvidence" fieldValue={this.state.user && this.state.user.cityOrProvidence ? this.state.user.cityOrProvidence : 'Empty' } updatePProfile={this.updateAllProfile}/>
+                          <ProfileField title="Country" fieldName="country" fieldValue={this.state.user && this.state.user.country ? this.state.user.country : 'Empty' } updatePProfile={this.updateAllProfile}/>
+                          <ProfileField title="Zip" fieldName="zipcode" fieldValue={this.state.user && this.state.user.zipcode ? this.state.user.zipcode : 'Empty' } updatePProfile={this.updateAllProfile}/>
                         </div>
-
                       </div>
-                    </Tab>
-                  </Tabs>
-                </div>}
-
-              </div>
-
+                    </div>
+                  </Tab>
+                </Tabs>
+              </div>}
             </div>
-
           </div>
-          : <div id="app" className="loader" /> }
-
         </div>
+        : <div id="app" className="loader" /> }
       </div>
     );
   }
